406cfd8a49dcaa92edd3eb09dbeb8de94bbf12eb
Daniel García <dgpelaez@minsait.com>
2025-05-29 19:19:17 +0200

    App android creada con capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'mascot-anuncios',
+  webDir: 'dist/mascot-anuncios/browser',
+};
+
+export default config;
