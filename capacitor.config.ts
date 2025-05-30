ba616e4a410971b575e9dd54791cf5e8fc080325
Daniel García <dgpelaez@minsait.com>
2025-05-30 17:59:07 +0200

    Eliminada app android

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'mascot-anuncios',
-  webDir: 'dist/mascot-anuncios/browser',
-};
-
-export default config;
